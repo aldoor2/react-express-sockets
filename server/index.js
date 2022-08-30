6aba55d8a0185ba18e2112fa2470cbda0c1534bd
Alphonse <apuertaj2@gmail.com>
2022-08-29 20:15:41 -0500

    Setup of server initial

---
+++ server/index.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import morgan from 'morgan'
+import { Server as SocketServer } from 'socket.io';
+import http from 'http'
+import cors from 'cors'
+
+import { PORT } from './config.js'
+
+const app = express()
+const server = http.createServer(app)
+const io = new SocketServer(server)
+
+app.use(cors())
+app.use(morgan('dev'))
+
+server.listen(PORT)
+console.log(`✔ Server running on port ${PORT}`)
